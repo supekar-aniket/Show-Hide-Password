f567096f70bb974258ff5fd58d90c864a8214270
Aniket Supekar <supekaraniket34@gmail.com>
2024-08-06 20:37:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [passStatus,setPassStatus] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <input type={passStatus ? 'text' : 'password'} className='textBox'/> 
+      <button className='btn' onClick={()=>{setPassStatus(!passStatus)}}>
+        {passStatus ? 'Hide' : 'Show'}
+      </button>
+
     </div>
   );
 }
